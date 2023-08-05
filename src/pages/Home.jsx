aa9df497be98ba6853060efaac433611a2d2eeea
Monzurulislampro <mounzrul@gmail.com>
2023-08-05 11:50:22 +0600

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import About from "../Components/About/About";
+import Hero from "../Components/Hero/Hero";
+import Navbar from "../Components/Navbar/Navbar";
+import Count from "../Components/count/Count";
+import Features from "../Components/features/Features";
+import WhyUs from "../Components/whyUs/WhyUs";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Hero></Hero>
+      <About></About>
+      <Count></Count>
+      <WhyUs></WhyUs>
+      <Features></Features>
+    </div>
+  );
+};
+
+export default Home;
